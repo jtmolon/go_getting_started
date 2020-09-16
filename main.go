570de2e1ded4f7b2e312c815dc50c316e317a737
Joao Molon <joao.molon@openapp.ie>
2020-09-16 23:39:15 +0200

    Initial commit

---
+++ main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/pluralsight/webservice/controllers"
+)
+
+func main() {
+	// u := models.User{
+	// 	ID:        2,
+	// 	FirstName: "Tricia",
+	// 	LastName:  "McMillan",
+	// }
+	// fmt.Println(u)
+	controllers.RegisterControllers()
+	http.ListenAndServe(":3000", nil)
+}
