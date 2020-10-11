c4c0e6179eda1cb9d6bbc37d171266d6a0a49c5e
João Molon <jtmolon@gmail.com>
2020-10-11 20:12:30 +0200

    Further improvements on controllers and models

--- main.go
+++ main.go
@@ -7,12 +7,6 @@ import (
 )
 
 func main() {
-	// u := models.User{
-	// 	ID:        2,
-	// 	FirstName: "Tricia",
-	// 	LastName:  "McMillan",
-	// }
-	// fmt.Println(u)
 	controllers.RegisterControllers()
 	http.ListenAndServe(":3000", nil)
 }
